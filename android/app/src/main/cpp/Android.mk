46525757c32008756e6c2c8d49bb7210729c63c1
Ryan <155229451+Ooonana@users.noreply.github.com>
2026-09-18 10:55:38 +0900

    Align bootstrap JNI for 16 KiB Android pages

--- android/app/src/main/cpp/Android.mk
+++ android/app/src/main/cpp/Android.mk
@@ -2,4 +2,5 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libalpine-bootstrap
 LOCAL_SRC_FILES := alpine-bootstrap-zip.S alpine-bootstrap.c
+LOCAL_LDFLAGS += -Wl,-z,max-page-size=16384
 include $(BUILD_SHARED_LIBRARY)
